500acd8d2302e819ecf2e7e9167e30bfa629da27
Luis García <aig.luis@gmail.com>
2020-11-24 15:09:08 -0600

    Agregada pregunta y states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useState } from "react";
+import Pregunta from "./components/pregunta";
+
 function App() {
+  //definir state
+  const [presupuesto, guardarPresupuesto] = useState(0);
+  const [restante, guardarRestante] = useState(0);
+
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
+        <div className="contenido-principal contenido">
+          <Pregunta
+            guardarPresupuesto={guardarPresupuesto}
+            guardarRestante={guardarRestante}
+          />
+        </div>
       </header>
     </div>
   );
--- src/components/pregunta.js
+++ src/components/pregunta.js
@@ -1,9 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Error from "./error";
 
-const Pregunta = () => {
+const Pregunta = ({ guardarPresupuesto, guardarRestante }) => {
+  //definit el state
+  const [cantidad, guardarCantidad] = useState(0);
+  const [error, guardarError] = useState(false);
+  //funcion que lee el presupuesto
+  const definirPresupuesto = (e) => {
+    guardarCantidad(parseInt(e.target.value, 10));
+  };
+
+  //Submit para definir presupuesto
+  const agregarPresupuesto = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (cantidad <= 0 || isNaN(cantidad)) {
+      guardarError(true);
+      return;
+    }
+
+    //Si pasa la validacion
+    guardarError(false);
+    guardarPresupuesto(cantidad);
+    guardarRestante(cantidad);
+  };
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
+
+      {error ? <Error mensaje="El Presupuesto es Incorrecto" /> : null}
+
+      <form onSubmit={agregarPresupuesto}>
+        <input
+          type="numbre"
+          className="u-full-width"
+          placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
+        />
+        <input
+          type="submit"
+          className="button-primary u-full-width"
+          value="Definir Presupuesto"
+        />
+      </form>
     </Fragment>
   );
 };
