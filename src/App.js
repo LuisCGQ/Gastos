56d3f54cdef54acb8c0031a7d63577a97972b03e
Luis García <aig.luis@gmail.com>
2020-11-26 11:59:36 -0600

    Agregadas funciones y States al formulario

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
-import Pregunta from "./components/pregunta";
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
 
 function App() {
   //definir state
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+
+  //cuando agregamos nuevo gasto
+  const agregarNuevoGasto = (gasto) => {
+    guardarGastos([...gastos, gasto]);
+  };
 
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
+          {mostrarPregunta ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Formulario agregarNuevoGasto={agregarNuevoGasto} />
+              </div>
+              <div className="one-half column">2</div>
+            </div>
+          )}
         </div>
       </header>
     </div>
